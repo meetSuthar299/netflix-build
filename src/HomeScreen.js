0963b470f868e3fea7472b1d0acf13307be25e8c
Meet Suthar <meetsuthar299@gmail.com>
2023-10-15 16:06:53 -0600

    Initial Setup
    Added FireBase. cleaned up not needed files. planned out home screen.

---
+++ src/HomeScreen.js
@@ -0,0 +1,14 @@
+ import React from "react";
+ import "./HomeScreen.css";
+
+ function HomeScreen() {
+    return (
+        <div className="homeScreen">Netflix
+            {/* Nav */}
+            {/* Banner */}
+            {/* Row */}
+        </div>
+    )
+ }
+
+ export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7GabKg0MrGjshuu76dyDiDpV9Egy1hlM",
+    authDomain: "netflix-clone-a23bc.firebaseapp.com",
+    projectId: "netflix-clone-a23bc",
+    storageBucket: "netflix-clone-a23bc.appspot.com",
+    messagingSenderId: "74480555678",
+    appId: "1:74480555678:web:83e89f5e0ba992559fcd8f"
+  };
